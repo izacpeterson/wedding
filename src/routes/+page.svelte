e9cf3a4ca1006ca5be6f5ecf31c2947670d1bec3
izacpeterson <izacpeterson@gmail.com>
2023-09-21 23:50:17 -0600

    styiling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <main>
+  <img src="/pines.png" alt="" />
   <div class="title">
     <h1>Rachel and Izac</h1>
-    <h2>Wedding</h2>
+    <!-- <h2>Wedding</h2> -->
   </div>
   <div class="details">
+    <h3>Wedding Reception:</h3>
     <h4>October 16th, 2023</h4>
     <h4>6pm - 8pm</h4>
     <h4>50 West 500 North, Lehi, Utah</h4>
   </div>
   <ul class="links">
+    <h3>Links:</h3>
     <li class="link"><a href="https://www.amazon.com/wedding/share/rachelizac">Amazon Registry</a></li>
     <li class="link"><a href="https://www.google.com/maps/place/50+W+500+N,+Lehi,+UT+84043/@40.3939417,-111.8528229,17z/data=!3m1!4b1!4m5!3m4!1s0x874d802ca4b10763:0xcd3c5c7f3b48a412!8m2!3d40.3939376!4d-111.850248?entry=ttu">Reception Location</a></li>
   </ul>
@@ -20,7 +23,6 @@
     padding: 0;
     box-sizing: border-box;
     font-weight: 100;
-    color: #062c13;
   }
   main {
     display: flex;
@@ -39,18 +41,24 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin: 10px;
   }
   li {
     list-style: none;
-    margin: 30px;
+    margin-bottom: 10px;
   }
   a {
     list-style: none;
-    text-decoration: none;
+    color: #0e642c;
+
+    /* text-decoration: none;
     background-color: #062c13;
     color: white;
     padding: 15px;
     border-radius: 5px;
-    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75); */
+  }
+  img {
+    width: 250px;
   }
 </style>
