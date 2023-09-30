5777f2b07e9a7ceb713e7770ba5fa93b4f9fbe7a
Izac Peterson <izacpeterson@gmail.com>
2023-09-30 17:33:56 -0600

    fixes

---
+++ src/lib/upload.svelte
@@ -0,0 +1,6 @@
+<div class="bg-[rgba(0,0,0,0.4)] backdrop-blur-sm w-full h-full fixed top-0 left-0">
+  <div class="bg-white m-16 p-4 flex flex-col items-center rounded-lg shadow">
+    <input type="file" />
+    <button class=" bg-green-900 text-white p-2 rounded shadow-lg">Upload</button>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,44 @@
+<script>
+  import Upload from "$lib/upload.svelte";
+  import { onMount } from "svelte";
+
+  let showUpload = false;
+
+  onMount(() => {
+    var data = {
+      content: "New Visitor on Wedding " + new Date().toLocaleTimeString(),
+    };
+    var options = {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("https://discordapp.com/api/webhooks/770724245214986261/qQTsNZsFXoiRopoc6EB3p0Ixt7427RkLd-BB_Cf4xKcWoOhFmvV1__2wJu590gkEAwmz", options);
+  });
+</script>
+
+{#if showUpload}
+  <Upload />
+{/if}
+
 <main class="flex flex-col items-center">
   <div class="flex flex-col items-center p-8">
-    <h3 class="text-xl">Wedding Reception:</h3>
+    <h2 class="text-xl">Wedding Reception:</h2>
     <h4>October 16th, 2023</h4>
     <h4>6pm - 8pm</h4>
     <h4>50 West 500 North, Lehi, Utah</h4>
   </div>
-  <a href="/rsvp" class="bg-green-900 p-2 rounded text-white">Let us know you're coming!</a>
+  <!-- <a href="/rsvp" class="bg-green-900 p-2 rounded text-white">Let us know you're coming!</a> -->
   <ul class="flex flex-col items-center p-8">
-    <h3>Links:</h3>
-    <li class="link"><a href="https://www.amazon.com/wedding/share/rachelizac">Amazon Registry</a></li>
-    <li class="link"><a href="https://www.google.com/maps/place/50+W+500+N,+Lehi,+UT+84043/@40.3939417,-111.8528229,17z/data=!3m1!4b1!4m5!3m4!1s0x874d802ca4b10763:0xcd3c5c7f3b48a412!8m2!3d40.3939376!4d-111.850248?entry=ttu">Reception Location</a></li>
+    <h2 class="text-xl">Links:</h2>
+    <li class="text-green-800"><a href="https://www.amazon.com/wedding/share/rachelizac">Amazon Registry</a></li>
+    <li class="text-green-800"><a href="https://www.google.com/maps/place/50+W+500+N,+Lehi,+UT+84043/@40.3939417,-111.8528229,17z/data=!3m1!4b1!4m5!3m4!1s0x874d802ca4b10763:0xcd3c5c7f3b48a412!8m2!3d40.3939376!4d-111.850248?entry=ttu">Reception Location</a></li>
   </ul>
-</main>
-<!-- 
-<style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-weight: 100;
-  }
-  main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-  div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-  }
-  ul {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 10px;
-  }
-  li {
-    list-style: none;
-    margin-bottom: 10px;
-  }
-  a {
-    list-style: none;
-    color: #0e642c;
 
-    /* text-decoration: none;
-    background-color: #062c13;
-    color: white;
-    padding: 15px;
-    border-radius: 5px;
-    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75); */
-  }
-  img {
-    width: 250px;
-  }
-</style>
- -->
+  <!-- <div class="flex flex-col items-center">
+    <h2 class="text-xl">Have a great picture of us? Feel free to share!</h2>
+    <button on:click={() => (showUpload = !showUpload)} class="bg-green-900 mt-2 p-2 rounded text-white">Upload Pictures</button>
+  </div> -->
+</main>
