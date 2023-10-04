4179409d9a8eba7fd2ec57ba289ccd708e6d60b5
izacpeterson <izacpeterson@gmail.com>
2023-10-03 23:18:53 -0600

    link click notify and visitor count

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,6 +24,20 @@
     };
     // fetch("https://discordapp.com/api/webhooks/770724245214986261/qQTsNZsFXoiRopoc6EB3p0Ixt7427RkLd-BB_Cf4xKcWoOhFmvV1__2wJu590gkEAwmz", options);
   });
+
+  function notify(msg) {
+    var data = {
+      content: msg,
+    };
+    var options = {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("https://discordapp.com/api/webhooks/770724245214986261/qQTsNZsFXoiRopoc6EB3p0Ixt7427RkLd-BB_Cf4xKcWoOhFmvV1__2wJu590gkEAwmz", options);
+  }
 </script>
 
 {#if showUpload}
@@ -44,9 +58,30 @@
   <!-- <a href="/rsvp" class="bg-green-900 p-2 rounded text-white">Let us know you're coming!</a> -->
   <ul class="flex flex-col items-center p-2">
     <h2 class="text-xl">Links:</h2>
-    <li class="text-green-800 m-2"><a href="https://www.amazon.com/wedding/share/rachelizac">Amazon Registry</a></li>
-    <li class="text-green-800 m-2"><a href="https://www.google.com/maps/place/50+W+500+N,+Lehi,+UT+84043/@40.3939417,-111.8528229,17z/data=!3m1!4b1!4m5!3m4!1s0x874d802ca4b10763:0xcd3c5c7f3b48a412!8m2!3d40.3939376!4d-111.850248?entry=ttu">Reception Location</a></li>
-    <li class="text-green-800 m-2"><a href="https://account.venmo.com/u/Izac-Peterson">Venmo</a></li>
+    <li class="text-green-800 m-2">
+      <a
+        href="https://www.amazon.com/wedding/share/rachelizac"
+        on:click={() => {
+          notify("Someone clicked the Amazon Registry link");
+        }}>Amazon Registry</a
+      >
+    </li>
+    <li class="text-green-800 m-2">
+      <a
+        href="https://www.google.com/maps/place/50+W+500+N,+Lehi,+UT+84043/@40.3939417,-111.8528229,17z/data=!3m1!4b1!4m5!3m4!1s0x874d802ca4b10763:0xcd3c5c7f3b48a412!8m2!3d40.3939376!4d-111.850248?entry=ttu"
+        on:click={() => {
+          notify("Someone clicked the Google Maps link");
+        }}>Reception Location</a
+      >
+    </li>
+    <li class="text-green-800 m-2">
+      <a
+        href="https://account.venmo.com/u/Izac-Peterson"
+        on:click={() => {
+          notify("Someone clicked the Venmo link");
+        }}>Venmo</a
+      >
+    </li>
   </ul>
 
   <div class="flex flex-col items-center">
--- src/routes/visitors/+page.svelte
+++ src/routes/visitors/+page.svelte
@@ -2,15 +2,19 @@
   import { getVisitors } from "$lib/firebase.js";
 
   let visitors = [];
+  let visitorCount = 0;
 
   getVisitors().then((data) => {
     visitors = data;
     console.log(visitors);
+    visitors.forEach((visitor) => {
+      if (visitor.ip !== "75.169.29.49" && visitor.city !== "Ashburn") visitorCount += 1;
+    });
   });
 </script>
 
-<div class="flex flex-col justify-center">
-  <h2 class="text-2xl m-2 p-2">Visitors</h2>
+<div class="flex flex-col justify-center items-center">
+  <h2 class="text-2xl m-2 p-2">Visitors: {visitorCount}</h2>
   {#each visitors as visitor}
     {#if visitor.ip !== "75.169.29.49" && visitor.city !== "Ashburn"}
       <div class="bg-white p-2 rounded flex flex-col items-center">
